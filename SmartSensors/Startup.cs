e23206ffc7269fa6ea619c1d5a88746d2722b281
Atanas Terziev <djnasi4@gmail.com>
2017-10-30 18:43:45 +0200

    Added intial project

---
+++ SmartSensors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartSensors.Startup))]
+namespace SmartSensors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
