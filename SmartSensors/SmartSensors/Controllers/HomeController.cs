2ada14e1a9564c9692e63fc85bcfe8f434554776
Milena Ivanova <milena_ii@abv.bg>
2017-11-06 16:18:09 +0200

    Admin in Database

--- SmartSensors/SmartSensors/Controllers/HomeController.cs
+++ SmartSensors/SmartSensors/Controllers/HomeController.cs
@@ -1,22 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SmartSensors.Models;
+using SmartSensors.Data;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 
 namespace SmartSensors.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationUserManager userManager;
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeController(ApplicationUserManager userManager, ApplicationDbContext dbContext)
+        {
+            this.userManager = userManager;
+            this.dbContext = dbContext;
+        }
+
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [Authorize]
+        public async Task<ActionResult> About()
         {
             ViewBag.message = "your application description page.";
+
+            //this.dbContext.Roles.Add(new IdentityRole() { Name = "Admin" });
+            //await this.dbContext.SaveChangesAsync();
+
+            //var user = await this.userManager.FindByNameAsync(this.User.Identity.Name);
+            //await this.userManager.AddToRoleAsync(user.Id, "Admin");
+
             return View();
         }
 
--- SmartSensors/SmartSensors/Controllers/RequestController.cs
+++ SmartSensors/SmartSensors/Controllers/RequestController.cs
@@ -9,23 +9,23 @@ namespace SmartSensors.Controllers
     public class RequestController : Controller
     {
         // GET: Request
-        public ActionResult GetSensors()
-        {
-            var webRequest = System.Net.WebRequest.Create("http://telerikacademy.icb.bg/api/sensor/d5d37a46-8ab5-41ec-b7d5-d28c2fd68d3d");
-            if (webRequest != null)
-            {
-                webRequest.Method = "GET";
-                webRequest.ContentType = "application/json";
-                webRequest.Headers["auth-token"] = "8e4c46fe-5e1d-4382-b7fc-19541f7bf3b0";
-                using (System.IO.Stream s = webRequest.GetResponse().GetResponseStream())
-                {
-                    using (System.IO.StreamReader sr = new System.IO.StreamReader(s))
-                    {
-                        var jsonResponse = sr.ReadToEnd();
-                    }
-                }
-            }
-            return View();
-        }
+        //public ActionResult GetSensors()
+        //{
+        //    var webRequest = System.Net.WebRequest.Create("http://telerikacademy.icb.bg/api/sensor/d5d37a46-8ab5-41ec-b7d5-d28c2fd68d3d");
+        //    if (webRequest != null)
+        //    {
+        //        webRequest.Method = "GET";
+        //        webRequest.ContentType = "application/json";
+        //        webRequest.Headers["auth-token"] = "8e4c46fe-5e1d-4382-b7fc-19541f7bf3b0";
+        //        using (System.IO.Stream s = webRequest.GetResponse().GetResponseStream())
+        //        {
+        //            using (System.IO.StreamReader sr = new System.IO.StreamReader(s))
+        //            {
+        //                var jsonResponse = sr.ReadToEnd();
+        //            }
+        //        }
+        //    }
+        //    return View();
+        //}
     }
 }
