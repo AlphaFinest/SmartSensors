5a0c4ca422352bba05a7670049bc4ca753a4f376
Milena Ivanova <milena_ii@abv.bg>
2017-11-07 15:00:13 +0200

    Admin Area

---
+++ SmartSensors/SmartSensors.Tests/Areas/Admin/Controllers/AdminControllerTests/AllUsers_Should.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using EntityFramework.Testing;
+using SmartSensors.Data.Models;
+using System.Collections.Generic;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using SmartSensors.Areas.Admin.Controllers;
+using SmartSensors.Areas.Admin.Models;
+using TestStack.FluentMVCTesting;
+
+namespace SmartSensors.Tests.Areas.Admin.Controllers.AdminControllerTests
+{
+    [TestClass]
+    public class AllUsers_Should
+    {
+        [TestMethod]
+        public void ReturnDefaultViewWithCorrectViewModel()
+        {
+            //Arange
+            var storeMock = new Mock<IUserStore<ApplicationUser>>();
+            var userManagerMock = new Mock<ApplicationUserManager>(storeMock.Object);
+            var users = new List<ApplicationUser>()
+            {
+                new ApplicationUser(){ UserName = "firstUser"},
+                new ApplicationUser(){ UserName = "secondUser"},
+            }.AsQueryable();
+
+            userManagerMock.Setup(m => m.Users).Returns(users);
+
+            var resultViewModel = users.Select(UserViewModel.Create).ToList();
+
+            AdminController controller = new AdminController(userManagerMock.Object);
+
+            //Act & Assert
+            controller
+                .WithCallTo(c => c.AllUsers())
+                .ShouldRenderDefaultView()
+                .WithModel<List<UserViewModel>>(viewModel =>
+                {
+                    for (int i = 0; i < viewModel.Count; i++)
+                    {
+                        Assert.AreEqual(resultViewModel[i].Username, viewModel[i].Username);
+                    }
+                });
+        }
+    }
+}
--- SmartSensors/SmartSensors/Areas/Admin/Models/UserViewModel.cs
+++ SmartSensors/SmartSensors/Areas/Admin/Models/UserViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using SmartSensors.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
+using System;
+using SmartSensors.Data.Models;
 
 namespace SmartSensors.Areas.Admin.Models
 {
     public class UserViewModel
     {
         public string Username { get; set; }
+
+        public static Expression<Func<ApplicationUser, UserViewModel>> Create
+        {
+            get
+            {
+                return u => new UserViewModel()
+                {
+                    Username = u.UserName
+                };
+            }
+        }
     }
 }
