84728c07790990d45c594ce63eb3b91358dc3180
Milena Ivanova <milena_ii@abv.bg>
2017-11-08 18:19:50 +0200

    All working

---
+++ SmartSensors/SmartSensors/Models/PublicViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using SmartSensors.Data.Models;
+
+namespace SmartSensors.Models
+{
+    public class PublicViewModels
+    {
+        public User Owner { get; set; }
+
+        public string SensorName { get; set; }
+
+        public string Value { get; set; }
+
+        public string ValueType { get; set; }
+
+    }
+}
