aca7304bd5177095e78ca6574672b951b97af551
Milena Ivanova <milena_ii@abv.bg>
2017-11-08 14:57:02 +0200

    Final Merge Resolve

--- SmartSensors/SmartSensors/Areas/Admin/Models/UserViewModel.cs
+++ SmartSensors/SmartSensors/Areas/Admin/Models/UserViewModel.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web;
-using System;
-using SmartSensors.Data.Models;
 
 namespace SmartSensors.Areas.Admin.Models
 {
