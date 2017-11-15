6fe998b73db8a066b462b926a96c0f30b30e2ce6
Milena Ivanova <milena_ii@abv.bg>
2017-11-15 18:08:08 +0200

    Admin All sensors unit tests

--- SmartSensors/SmartSensors.Tests/Areas/Admin/Controllers/AdminControllerTests/AllSensors_Should.cs
+++ SmartSensors/SmartSensors.Tests/Areas/Admin/Controllers/AdminControllerTests/AllSensors_Should.cs
@@ -1,57 +1,57 @@
-﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Moq;
-//using SmartSensors.Data.Models;
-//using System.Collections.Generic;
-//using Microsoft.AspNet.Identity;
-//using System.Linq;
-//using SmartSensors.Areas.Admin.Controllers;
-//using TestStack.FluentMVCTesting;
-//using SmartSensors.Service.Contracts;
-//using SmartSensors.Data;
-//using SmartSensors.Service.ViewModels;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SmartSensors.Data.Models;
+using System.Collections.Generic;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using SmartSensors.Areas.Admin.Controllers;
+using TestStack.FluentMVCTesting;
+using SmartSensors.Service.Contracts;
+using SmartSensors.Data;
+using SmartSensors.Service.ViewModels;
 
-//namespace SmartSensors.Tests.Areas.Admin.Controllers.AdminControllerTests
-//{
-//    [TestClass]
-//    public class AllSensors_Should
-//    {
-//        [TestMethod]
-//        public void ReturnTheCorrectViewModel()
-//        {
-//            //Arrange
-//            var sensorServiceMock = new Mock<ISensorService>();
-//            var userServiceMock = new Mock<IUserService>();
-//            var dbContextMock = new Mock<ApplicationDbContext>();
-//            var urlProviderMock = new Mock<IUrlProvider>();
-//            var userManagerMock = new Mock<ApplicationUserManager>();
+namespace SmartSensors.Tests.Areas.Admin.Controllers.AdminControllerTests
+{
+    [TestClass]
+    public class AllSensors_Should
+    {
+        [TestMethod]
+        public void ReturnTheCorrectViewModel()
+        {
+            //Arrange
+            var sensorServiceMock = new Mock<ISensorService>();
+            var userServiceMock = new Mock<IUserService>();
+            var dbContextMock = new Mock<ApplicationDbContext>();
+            var urlProviderMock = new Mock<IUrlProvider>();
+            var userManagerMock = new Mock<ApplicationUserManager>();
 
-//            var sensors = new List<FullSensorViewModel>()
-//            {
-//                new FullSensorViewModel(){ Name = "Sensor1" },
-//                new FullSensorViewModel(){ Name = "Sensor2" }
-//            };
+            var sensors = new List<FullSensorViewModel>()
+            {
+                new FullSensorViewModel(){ Name = "Sensor1" },
+                new FullSensorViewModel(){ Name = "Sensor2" }
+            };
 
-//            var user = new User() { UserName = "FirstUser" };
+            var user = new User() { UserName = "FirstUser" };
 
-//            sensorServiceMock.Setup(s => s.GetAllSensors()).Returns(sensors);
+            sensorServiceMock.Setup(s => s.GetAllSensors()).Returns(sensors);
 
-//            var controller = new AdminController(dbContextMock.Object, userServiceMock.Object, sensorServiceMock.Object, urlProviderMock.Object);
+            var controller = new AdminController(dbContextMock.Object, userServiceMock.Object, sensorServiceMock.Object, urlProviderMock.Object);
 
-//            //Act & Assert
-//            controller
-//                .WithCallTo(c => c.AllSensors())
-//                .ShouldRenderDefaultView()
-//                .WithModel<List<FullSensorViewModel>>
-//            (viewModel =>
-//            {
-//                for (int i = 0; i < viewModel.Count; i++)
-//                {
-//                    Assert.AreEqual(viewModel[i].Name, sensors[i].Name);
-//                }
-//            });
+            //Act & Assert
+            controller
+                .WithCallTo(c => c.AllSensors())
+                .ShouldRenderDefaultView()
+                .WithModel<List<FullSensorViewModel>>
+            (viewModel =>
+            {
+                for (int i = 0; i < viewModel.Count; i++)
+                {
+                    Assert.AreEqual(viewModel[i].Name, sensors[i].Name);
+                }
+            });
 
 
 
-//        }
-//    }
-//}
+        }
+    }
+}
